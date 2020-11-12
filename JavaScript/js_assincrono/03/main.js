bba28cee1b468e03267ce661012f47d28cca71d6
Paulo <pmatos2000@gmail.com>
2020-11-12 11:58:00 -0300

    Aula 03 - Axios

---
+++ JavaScript/js_assincrono/03/main.js
@@ -0,0 +1,11 @@
+axios.get("https://api.github.com/users/pmatos2000")
+    .then(
+        function(response){
+            console.log(response);
+        }
+    )
+    .catch(
+        function(error){
+            console.log(error);
+        }
+    );
