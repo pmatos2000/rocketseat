a934cfa848f0ced643c7f1913bf5496cbfd0f909
Paulo <pmatos2000@gmail.com>
2020-11-12 10:08:49 -0300

    Aula 03 - Renderizando Todos

---
+++ JavaScript/app_de_todos/03/todos.js
@@ -0,0 +1,21 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttomElement = document.querySelector("#app button");
+
+var todos = [
+    "Fazer café",
+    "Estudar Javascript",
+    "Acessar comunidade da Rockestseat"
+];
+
+function renderTodos(){
+    for(todo of todos){
+        var todoElement = document.createElement("li");
+        var todoText = document.createTextNode(todo);
+        
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
