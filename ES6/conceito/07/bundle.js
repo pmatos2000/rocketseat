f260cb3c85859d03c49a3e278823dcb77fa136f8
Paulo <pmatos2000@gmail.com>
2020-11-13 14:10:51 -0300

    Arrow functions

---
+++ ES6/conceito/07/bundle.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var arr = [1, 3, 4, 5, 8, 9];
+/*
+const newArr = arr.map( function(item){
+    return item * 2;
+});
+*/
+
+var newArr = arr.map(function (item) {
+  return item * 2;
+});
+console.log(newArr);
+
+var teste = function teste() {
+  return {
+    nome: "Diego"
+  };
+};
+
+console.log(teste());
---
+++ ES6/conceito/07/main.js
@@ -0,0 +1,14 @@
+const arr = [1, 3, 4, 5, 8, 9];
+
+/*
+const newArr = arr.map( function(item){
+    return item * 2;
+});
+*/
+
+const newArr = arr.map( item => item * 2 );
+console.log(newArr);
+
+const teste = () => ({nome: "Diego"});
+
+console.log(teste());
