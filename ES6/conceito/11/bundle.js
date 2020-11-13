ea719b27288ba66e2b4692593f8a238d43e6b299
Paulo <pmatos2000@gmail.com>
2020-11-13 16:25:40 -0300

    Aula 11 - Literais

---
+++ ES6/conceito/11/bundle.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var nome = "Diego";
+var idade = 23; //console.log("Meu nome é " + nome + " e tenho " + idade + " anos.");
+
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos"));
