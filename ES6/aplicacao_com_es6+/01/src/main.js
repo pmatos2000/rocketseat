e3ee81f41dd668f1bd4f765cdeb1de81b9c2d807
pmatos2000 <pmatos2000@gmail.com>
2020-11-17 14:06:04 -0300

    Aula 01 - Estrutura e estilo

---
+++ ES6/aplicacao_com_es6+/01/src/main.js
@@ -0,0 +1,114 @@
+// Funão delay aciona o .then após 1s
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+/*
+function umPorSegundo() {
+    delay().then(() => {
+        console.log('1s');
+        delay().then(() => {
+            console.log('2s');
+            delay().then(() => {
+                console.log('3s');
+            });
+        })
+    });
+}
+*/
+
+async function umPorSegundo(){
+    await delay();
+    console.log('1s');
+    await delay();
+    console.log('2s');
+    await delay();
+    console.log('3s')
+}
+
+umPorSegundo();
+
+//--------------------------------------------------
+
+
+import axios from 'axios';
+
+/*
+function getUserFromGithub(user){
+    axios.get(`https://api.github.com/users/${user}`)
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(err => {
+        console.log('Usuário não existe');
+    })
+}
+*/
+
+async function getUserFromGithub(user){
+    try{
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+        console.log(response);
+    }
+    catch (err){
+        console.log('Usuário não existe');
+    }
+}
+
+getUserFromGithub('diego3g');
+getUserFromGithub('diego3g124123');
+
+//--------------------------------------------------
+
+/*
+class Github{
+    static getRepositories(repo){
+        axios.get(`https://api.github.com/repos/${repo}`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(err => {
+                console.log('Repositório não existe');
+            });
+    }
+}
+*/
+
+class Github{
+    static async getRepositories(repo){
+        try{
+            const response =  await axios.get(`https://api.github.com/repos/${repo}`);
+        }
+        catch(err){
+            console.log('Repositório não existe');
+        }
+    }
+}
+
+
+Github.getRepositories('rocketseat/rocketseat.com.br');
+Github.getRepositories('rocketseat/dslkvmskv');
+
+
+//-----------------------------------------------------
+
+/*
+const buscaUsuario = usuario => {
+    axios.get(`https://api.github.com/users/${user}`)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.log('Usuário não existe');
+        });
+}
+*/
+
+const buscaUsuario = async () =>{
+    try{
+        const response = await axios.get(`https://api.github.com/users/${user}`);
+    }
+    catch(err){
+        console.log('Usuário não existe');
+    }
+}
+
+buscaUsuario('diego3g');
