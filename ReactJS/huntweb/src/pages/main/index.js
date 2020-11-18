d9321a94684a113f8f607e985dda176a96784aa3
pmatos2000 <pmatos2000@gmail.com>
2020-11-18 15:30:55 -0300

    Aula 08 - Pagina anterior/próxima

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -7,18 +7,45 @@ class Main extends Component {
 
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     };
 
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-        const response = await api.get("/products");
-        this.setState({products: response.data.docs})
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
+        const { docs, ...productInfo} = response.data;
+        this.setState({products: docs, productInfo, page})
+    };
+
+
+    prevPage = () => {
+        const { page } = this.state;
+
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+
+    };
+
+    nextPage = () =>  {
+        const {page, productInfo} = this.state;
+
+        if(page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
     };
 
     render(){
+        const {products, productInfo, page} = this.state;
+
         return(
             <div className="product-list">
                 { this.state.products.map( (product) => (
@@ -28,6 +55,10 @@ class Main extends Component {
                         <a href="">Acessar</a>
                     </article>
                 )) }
+                <div className="actions">
+                    <button onClick={this.prevPage} disabled={page===1}>Anterior</button>
+                    <button onClick={this.nextPage} disabled={page===productInfo.pages}>Proximo</button>
+                </div>
             </div>
         )
     }
