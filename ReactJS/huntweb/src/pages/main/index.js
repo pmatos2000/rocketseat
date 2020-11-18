6173eb655d4d7dd1ebc93eab5eb2dca5f5845aca
pmatos2000 <pmatos2000@gmail.com>
2020-11-18 11:41:46 -0300

    Aula 06 - Amazenamento de estado

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -3,19 +3,27 @@ import api from "../../services/api"
 
 class Main extends Component {
 
+    state = {
+        products: [],
+    };
+
     componentDidMount(){
         this.loadProducts();
     }
 
     loadProducts = async () => {
         const response = await api.get("/products");
-        console.log(response);
+        this.setState({products: response.data.docs})
     };
 
     render(){
         return(
-            <h1>Olá mundo</h1>
-        );
+            <div className="product-list">
+                { this.state.products.map( (product) => (
+                    <h2 key={product._id}>{product.title}</h2>
+                )) }
+            </div>
+        )
     }
 }
 
